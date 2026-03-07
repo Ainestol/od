2c27ecdf93cf5ab60a6cd671a444d0504e02d5db
Jiří <Ainestol@seznam.cz>
2026-03-07 23:14:00 +0100

    Login security improvements

--- api/list_my_bug_reports.php
+++ api/list_my_bug_reports.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 require_once __DIR__.'/../../api/admin/_bootstrap.php';
 assert_admin();
 header('Content-Type: application/json; charset=utf-8');
-session_start();
 
 if (empty($_SESSION['web_user_id'])) {
   http_response_code(401);
