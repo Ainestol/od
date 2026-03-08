d2b70e5efcc374b555ad312d28c5755ba093a9e3
Jiří <Ainestol@seznam.cz>
2026-03-08 19:09:07 +0100

    Login security improvements

--- admin/api/economy_search_user.php
+++ admin/api/economy_search_user.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 header('Content-Type: application/json');
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/db.php';
-require_once __DIR__ . '/_bootstrap.php';
+require_once __DIR__.'/../_bootstrap.php';
 
 
 if ($_SESSION['role'] !== 'admin') {
