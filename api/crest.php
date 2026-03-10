e30ee4edb23a27f2de0efc1e94e133866ec61a50
Jiří <Ainestol@seznam.cz>
2026-03-10 08:58:13 +0100

    Login security improvements

--- api/crest.php
+++ api/crest.php
@@ -24,6 +24,40 @@ if(!$row){
     exit;
 }
 
+$data = $row['data'];
+
+/* crest velikost v L2 */
+$width = 16;
+$height = 12;
+
+/* vytvoření BMP hlavičky */
+
+$filesize = 54 + strlen($data);
+
+$bmp  = "BM";
+$bmp .= pack("V", $filesize);
+$bmp .= pack("V", 0);
+$bmp .= pack("V", 54);
+$bmp .= pack("V", 40);
+$bmp .= pack("V", $width);
+$bmp .= pack("V", $height);
+$bmp .= pack("v", 1);
+$bmp .= pack("v", 8);
+$bmp .= pack("V", 0);
+$bmp .= pack("V", strlen($data));
+$bmp .= pack("V", 0);
+$bmp .= pack("V", 0);
+$bmp .= pack("V", 256);
+$bmp .= pack("V", 0);
+
+/* grayscale paleta */
+
+for ($i = 0; $i < 256; $i++) {
+    $bmp .= chr($i) . chr($i) . chr($i) . chr(0);
+}
+
+$bmp .= $data;
+
 header("Content-Type: image/bmp");
 
-echo $row['data'];
+echo $bmp;
