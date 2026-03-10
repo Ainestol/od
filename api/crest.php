a831654ef779f4c8fa2ac199fb22f964377d2b93
Jiří <Ainestol@seznam.cz>
2026-03-10 08:47:18 +0100

    Login security improvements

--- api/crest.php
+++ api/crest.php
@@ -1,69 +1,53 @@
 <?php
 
-require_once __DIR__ . '/../config/db_game.php';
+require_once __DIR__.'/../config/db_game.php';
 
 $id = intval($_GET['id'] ?? 0);
 
-if ($id <= 0) {
+if(!$id){
     http_response_code(404);
     exit;
 }
 
-try {
+$stmt = $pdoGame->prepare("
+SELECT data
+FROM crests
+WHERE crest_id = ?
+");
 
-    $stmt = $pdoGame->prepare("
-        SELECT data, type
-        FROM crests
-        WHERE crest_id = ?
-        LIMIT 1
-    ");
+$stmt->execute([$id]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $stmt->execute([$id]);
-
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!$row || empty($row['data'])) {
-        http_response_code(404);
-        exit;
-    }
-
-    $data = $row['data'];
-
-    /*
-     L2J někdy ukládá crest bez BMP hlavičky.
-     Pokud hlavička chybí, vytvoříme ji.
-    */
-
-    if (substr($data, 0, 2) !== "BM") {
-
-        $size = strlen($data) + 54;
-
-        $header =
-            "BM" .
-            pack("V", $size) .
-            pack("v", 0) .
-            pack("v", 0) .
-            pack("V", 54) .
-            pack("V", 40) .
-            pack("V", 16) .
-            pack("V", 12) .
-            pack("v", 1) .
-            pack("v", 24) .
-            pack("V", 0) .
-            pack("V", strlen($data)) .
-            pack("V", 0) .
-            pack("V", 0) .
-            pack("V", 0) .
-            pack("V", 0);
-
-        $data = $header . $data;
-    }
-
-    header("Content-Type: image/bmp");
-    echo $data;
-
-} catch (Exception $e) {
-
-    http_response_code(500);
-
+if(!$row){
+    http_response_code(404);
+    exit;
 }
+
+$data = $row['data'];
+
+/* crest rozměry */
+$width = 16;
+$height = 12;
+
+$image = imagecreatetruecolor($width,$height);
+
+/* jednoduchá grayscale paleta */
+for($y=0;$y<$height;$y++){
+    for($x=0;$x<$width;$x++){
+
+        $i = ($y*$width)+$x;
+
+        if(!isset($data[$i])) continue;
+
+        $val = ord($data[$i]);
+
+        $color = imagecolorallocate($image,$val,$val,$val);
+
+        imagesetpixel($image,$x,$y,$color);
+    }
+}
+
+header("Content-Type: image/png");
+
+imagepng($image);
+imagedestroy($image);
