cfd3d61f58bb77ec87cb7f0b28e777d4160e72d5
Jiří <Ainestol@seznam.cz>
2026-03-11 08:27:18 +0100

    Login security improvements

--- api/crest.php
+++ api/crest.php
@@ -55,13 +55,43 @@ for ($y = 0; $y < $height; $y++) {
             break;
         }
 
-        $val = ord($data[$offset]);
+     $blockX = intdiv($x,4);
+$blockY = intdiv($y,4);
 
-        $color = imagecolorallocate($img, $val, $val, $val);
+$blockIndex = ($blockY * 4 + $blockX) * 8;
 
-        imagesetpixel($img, $x, $y, $color);
+$c0 = unpack("v", substr($data,$blockIndex,2))[1];
+$c1 = unpack("v", substr($data,$blockIndex+2,2))[1];
 
-        $offset++;
+$r0 = (($c0 >> 11) & 31) << 3;
+$g0 = (($c0 >> 5) & 63) << 2;
+$b0 = ($c0 & 31) << 3;
+
+$r1 = (($c1 >> 11) & 31) << 3;
+$g1 = (($c1 >> 5) & 63) << 2;
+$b1 = ($c1 & 31) << 3;
+
+$colors = [
+    [$r0,$g0,$b0],
+    [$r1,$g1,$b1],
+    [($r0+$r1)/2,($g0+$g1)/2,($b0+$b1)/2],
+    [0,0,0]
+];
+
+$lookup = unpack("V", substr($data,$blockIndex+4,4))[1];
+
+$px = $x % 4;
+$py = $y % 4;
+
+$shift = ($py*4 + $px) * 2;
+
+$index = ($lookup >> $shift) & 3;
+
+$c = $colors[$index];
+
+$color = imagecolorallocate($img,$c[0],$c[1],$c[2]);
+
+imagesetpixel($img,$x,$y,$color);
     }
 }
 
