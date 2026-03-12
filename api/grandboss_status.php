3378eb0f8c81b81b05adfb88ee01d10102773c07
Jiří <Ainestol@seznam.cz>
2026-03-12 20:20:19 +0100

    Login security improvements

--- api/grandboss_status.php
+++ api/grandboss_status.php
@@ -1,7 +1,5 @@
 <?php
 
-header('Content-Type: application/json');
-
 require_once __DIR__.'/../config/db_game.php';
 
 try{
@@ -11,27 +9,28 @@ SELECT
 g.boss_id,
 b.name,
 b.level,
-MAX(g.respawn_time) AS respawn_time
+g.respawn_time,
+r.respawn,
+r.respawn_random
 FROM grandboss_data g
 LEFT JOIN boss_list b ON b.boss_id = g.boss_id
-WHERE b.type='grand'
-AND g.boss_id != 25512
-GROUP BY g.boss_id
+LEFT JOIN boss_respawn r ON r.boss_id = g.boss_id
+WHERE g.boss_id IN (29001,29006,29014,29020,29022,29028,29045,29068)
 ORDER BY b.level ASC
 ";
 
 $stmt = $pdoGame->query($sql);
 
 echo json_encode([
-    "ok" => true,
-    "data" => $stmt->fetchAll()
+    "ok"=>true,
+    "data"=>$stmt->fetchAll(PDO::FETCH_ASSOC)
 ]);
 
 }catch(Exception $e){
 
 echo json_encode([
-    "ok" => false,
-    "error" => $e->getMessage()
+    "ok"=>false,
+    "error"=>$e->getMessage()
 ]);
 
 }
