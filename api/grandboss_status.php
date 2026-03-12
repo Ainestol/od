19b6b6385b9814b6c4766c2b094373d293c684a2
Jiří <Ainestol@seznam.cz>
2026-03-12 19:57:37 +0100

    Login security improvements

--- api/grandboss_status.php
+++ api/grandboss_status.php
@@ -11,18 +11,30 @@ SELECT
 g.boss_id,
 b.name,
 b.level,
-MAX(g.respawn_time) as respawn_time
+MAX(g.respawn_time) AS respawn_time
 FROM grandboss_data g
 LEFT JOIN boss_list b ON b.boss_id = g.boss_id
-WHERE b.type='grand'
+WHERE g.boss_id IN (
+29001, -- Queen Ant
+29006, -- Core
+29014, -- Orfen
+29022, -- Zaken
+29020, -- Baium
+29019, -- Antharas
+29028, -- Valakas
+29045  -- Frintezza
+)
 GROUP BY g.boss_id
 ORDER BY b.level ASC
+";
 
 $stmt = $pdoGame->query($sql);
 
+$data = $stmt->fetchAll();
+
 echo json_encode([
     "ok" => true,
-    "data" => $stmt->fetchAll()
+    "data" => $data
 ]);
 
 }catch(Exception $e){
