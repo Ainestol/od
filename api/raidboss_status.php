d7b3013cc84a06911ead9f865a7d80128656c600
Jiří <Ainestol@seznam.cz>
2026-03-13 09:46:03 +0100

    Login security improvements

--- api/raidboss_status.php
+++ api/raidboss_status.php
@@ -8,29 +8,64 @@ try{
 
 $sql = "
 SELECT
-r.id AS boss_id,
-b.name,
-b.level,
-CASE
-    WHEN r.respawnTime = 0 THEN 0
-    ELSE FLOOR(r.respawnTime / 1000) + s.respawn_time
-END AS respawn_time,
-s.respawn_random
+    r.id AS boss_id,
+    b.name,
+    b.level,
+    FLOOR(r.respawnTime / 1000) AS kill_time
 FROM npc_respawns r
 LEFT JOIN boss_list b ON b.boss_id = r.id
-LEFT JOIN raidboss_spawnlist s ON s.boss_id = r.id
 WHERE b.type = 'raid'
 ORDER BY b.level ASC
 ";
 
 $stmt = $pdoGame->query($sql);
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$data = $stmt->fetchAll();
+
+/* -----------------------------
+   DEFAULT RAID BOSS RESPAWN
+   ----------------------------- */
+
+$respawn = 36 * 3600;   // 36h
+$random  = 24 * 3600;   // 24h
+
+
+foreach($data as &$b){
+
+    $kill = intval($b['kill_time']);
+
+    /* ochrana proti špatnému času */
+    if($kill > time()){
+        $kill = 0;
+    }
+
+    if($kill > 0){
+
+        $window_start = $kill + $respawn;
+        $window_end   = $window_start + $random;
+
+        $b['respawn_time']   = $window_start;
+        $b['respawn_random'] = $random;
+
+    }else{
+
+        /* boss alive */
+
+        $b['respawn_time']   = 0;
+        $b['respawn_random'] = 0;
+
+    }
+
+    unset($b['kill_time']);
+
+}
+
 
 echo json_encode([
     "ok" => true,
     "data" => $data
 ]);
+
 
 }catch(Exception $e){
 
