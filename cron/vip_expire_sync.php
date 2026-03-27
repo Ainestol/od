32147554497d92d2f10aa9a885c7363c088a2fdc
Jiří <Ainestol@seznam.cz>
2026-03-27 13:05:48 +0100

    Login security improvements

--- cron/vip_expire_sync.php
+++ cron/vip_expire_sync.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/db_game.php';
-
+$pdoGame = $gamePdo;
 // === CHAR VIP ===
 $st = $pdo->query("
     SELECT target_id
