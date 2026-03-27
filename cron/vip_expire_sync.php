2ee86402940068c4075a4d1a0db0051859976206
Jiří <Ainestol@seznam.cz>
2026-03-27 23:40:44 +0100

    Login security improvements

--- cron/vip_expire_sync.php
+++ cron/vip_expire_sync.php
@@ -1,11 +1,15 @@
 <?php
 
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../config/db_game.php';
-$pdoGame = $gamePdo;
-if (!$pdoGame) {
-    die('DB GAME not connected');
-}
+$pdoGame = new PDO(
+    'mysql:host=localhost;dbname=l2game;charset=utf8mb4',
+    'l2_reader',
+    '@Heslojeheslo08',
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+);
 
 
 
